7781d6a79158befdf80fde6d5ca50a641dea9f97
thespywholovedher <ahmal.jones@gmail.com>
2025-11-14 23:50:23 -0500

    updated refactored main a bit

--- src/main.zig
+++ src/main.zig
@@ -2,19 +2,38 @@ const std = @import("std");
 const t3d_zig = @import("t3d_zig");
 
 const sdl3 = @import("sdl3");
+const Window = sdl3.video.Window;
 
 const fps = 60;
-const screen_width = 640;
-const screen_height = 480;
+const window_title = "All your codebase are belong to us.";
+const window_width = 640;
+const window_height = 480;
+
+fn createWindow() !*Window {
+    return try Window.init(window_title, window_width, window_height);
+}
+
+fn closeWindow(window: *Window) !void {
+    window.deinit();
+}
+
+fn gameInit(init_flags: sdl3.InitFlags) !void {
+    try sdl3.init(init_flags);
+}
+
+fn gameLoop() void {}
+
+fn gameShutdown(init_flags: sdl3.InitFlags) void {
+    sdl3.shutdown();
+    sdl3.quit(init_flags);
+}
 
 pub fn main() !void {
-    defer sdl3.shutdown();
-
     const init_flags = sdl3.InitFlags{ .video = true };
-    try sdl3.init(init_flags);
-    defer sdl3.quit(init_flags);
 
-    const window = try sdl3.video.Window.init("All you codebase are belong to us.", screen_width, screen_height, .{});
+    try gameInit(init_flags);
+    defer gameShutdown(init_flags);
+    const window = try sdl3.video.Window.init("All you codebase are belong to us.", window_width, window_height, .{});
     defer window.deinit();
 
     var fps_cap = sdl3.extras.FramerateCapper(f32){ .mode = .{ .limited = fps } };
@@ -33,8 +52,14 @@ pub fn main() !void {
             switch (event) {
                 .quit => quit = true,
                 .terminating => quit = true,
+                .key_down => {
+                    const keyName = sdl3.keyboard.getKeyName(event.key_down.key.?) orelse "Unknown Key";
+                    std.debug.print("{s}", .{keyName});
+                },
                 else => {},
             };
+
+        gameLoop();
     }
 
     // Prints to stderr, ignoring potential errors.
